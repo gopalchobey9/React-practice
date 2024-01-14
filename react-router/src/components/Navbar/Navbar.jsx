976768bec113d33291f646b69e6f661adc338a01
gopal chobey <100458189+gopalchobey9@users.noreply.github.com>
2024-01-15 00:56:13 +0530

    Creating a dashboard and Navbar using react-router-dom

---
+++ react-router/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+       <h3>StudyNotes</h3>
+       <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+       </ul>
+      <div>
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
